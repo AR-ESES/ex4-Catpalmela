e73020d703db234b90781d265657f9410b0a4f1c
Catpalmela <catarina.palmela1@gmail.com>
2023-05-29 17:22:21 +0100

    Código

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let espLarg = 20;
-let espAltura = 20;
+let espLarg = 15;
+let espAltura = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(160);
   stroke(255);
   strokeWeight(2);
   noFill();
@@ -12,12 +12,26 @@ function setup() {
 
 function draw() {
   
-  for (let i = 1; i < espLarg; i++) {
-    for (let j = 1; j < espAltura; j++) {
-      push();
-      translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
-      pop();
+	for (let i = 1; i < espLarg; i++) {
+	  for (let j = 1; j < espAltura; j++) {
+		push();
+		translate(i * width / espLarg, j * height / espAltura)
+		translate(-55,-45)
+		scale(0.08);
+		noStroke();
+
+
+
+		noFill();
+		stroke(50,255);
+		circle(width/2,height /2,height/3);
+		circle(width/2-height/3/4, height/2-height/18, height/12);
+		circle(width/2+height/3/4, height/2-height/18, height/12);
+		ellipse(750,670, 200, 350);
+		ellipse(450,600,420,52)
+		ellipse(1040,600,400,52)
+	 
+	  pop();
     }
   }
 }
